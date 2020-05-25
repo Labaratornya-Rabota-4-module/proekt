1e04395913741a3ce7cac4bccbff73b491efb6bb
DashkoAnastasia <65736322+DashkoAnastasia@users.noreply.github.com>
2020-05-25 19:08:04 +0300

    Update OdeParser.h
    Класс, который разбирает начальные значения для ОДУ

--- OdeParser.h
+++ OdeParser.h
@@ -1,1 +1,45 @@
+#pragma once
+#include <map>
+#include <string>
+#include <fstream>
+
+
+
+// Класс который разбирает начальные данные для ОДУ
+class OdeParser
+{
+private:
+public:
+    OdeParser();
+    std::map<std::string, double> operator() (std::ifstream&); // Перегруженный оператор
+};
+
+OdeParser::OdeParser()
+{ }
+
+// Функция которая создает массив ключ-значение
+std::map<std::string, double> OdeParser::operator() (std::ifstream& file)
+{
+    std::map<std::string, double> map;
+    std::string pName;
+    std::string pValue;
+    
+    while (1)
+    {
+        std::getline(file, pName); // Берем строку файла
+        
+        if (file.eof()) break;
+        
+        size_t delimiter_position = pName.find('=');
+        if (delimiter_position != 0 && delimiter_position != std::string::npos) // Если корректный формат
+        {
+            pValue = pName.substr(delimiter_position + 1);
+            pName = pName.substr(0, delimiter_position);
+            
+            map[pName] = std::atof(pValue.c_str()); // Если не смогли привести к типу, то записывем ноль
+        }
+    }
+    
+    return map; // Возвращаем ассоциативный объект
+}
 
