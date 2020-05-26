8194accb83fb0f8ad3594f95f73f7356b5e1af25
arikhha <65591362+arikhha@users.noreply.github.com>
2020-05-26 14:56:21 +0700

    Create Job.h

---
+++ Job.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <fstream>
+
+// Абстрактная работа
+class Job
+{
+protected:
+	std::ifstream fileIn; // Файл, из которого необходимо считать начальные данные
+	std::ofstream fileOut; // Файл, в который необходимо записать результат
+public:
+	Job(const Job&) = delete;
+	// Конструктор
+	Job(std::ifstream&& a, std::ofstream&& b) : fileIn(std::move(a)), fileOut(std::move(b)) { }
+	// Функция, которая выполняет задание
+	virtual void makeJob() = 0;
+	virtual ~Job() { }
+};
